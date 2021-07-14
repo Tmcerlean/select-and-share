1fc02aebaa104669357c338476831d1a59df05a2
Thomas McErlean <tjmcerlean@googlemail.com>
2021-07-14 14:00:49 +0100

    add axios and configure geocode API call

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const form = document.querySelector("form");
+const addressInput = document.getElementById('address');
+function searchAddressHandler(event) {
+    event.preventDefault();
+}
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', searchAddressHandler);
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector("form");
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${process.env.GOOGLE_API_KEY}`
+    ).then(response => {
+        console.log(response);
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+form?.addEventListener('submit', searchAddressHandler)!;
